558c723bf4003dc3de394504d3f0475d121a990e
magiccrafter <magiccrafter@gmail.com>
2017-02-23 19:16:34 +0100

    Model

---
+++ src/main/java/org/googlehashcode/domain/VideoRequest.java
@@ -0,0 +1,17 @@
+package org.googlehashcode.domain;
+
+/**
+ * Created by magiccrafter on 2/23/2017.
+ */
+public class VideoRequest {
+
+    public final int videoId;
+    public final int endpointId;
+    public final int requestsNumber;
+
+    public VideoRequest(int videoId, int endpointId, int requestsNumber) {
+        this.videoId = videoId;
+        this.endpointId = endpointId;
+        this.requestsNumber = requestsNumber;
+    }
+}
