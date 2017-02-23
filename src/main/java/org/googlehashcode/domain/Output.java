0ce070c5a7a49370a78fad505abdb7a43a4a0f50
magiccrafter <magiccrafter@gmail.com>
2017-02-23 21:03:43 +0100

    Output datastructure

---
+++ src/main/java/org/googlehashcode/domain/Output.java
@@ -0,0 +1,29 @@
+package org.googlehashcode.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Created by magiccrafter on 2/23/2017.
+ */
+public class Output {
+
+    public final Map<Integer, List<Integer>> cacheServers = new HashMap<>();
+
+    public Output(List<Integer> serverIds) {
+        serverIds.forEach(id -> cacheServers.put(id, new ArrayList<>()));
+    }
+
+    public void addVideo(int serverId, int videoId) {
+        cacheServers.get(serverId).add(videoId);
+    }
+
+    public void removeVideo(int serverId, int videoId) {
+        List<Integer> updated = cacheServers.get(serverId)
+                .stream().filter(v -> v != videoId).collect(Collectors.toList());
+        cacheServers.put(serverId, updated);
+    }
+}
